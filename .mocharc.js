8236b0c30de02f5c5ba78dd1c4163fe6357a5bfe
Akshat Bansal <akshat2610@gmail.com>
2021-03-27 07:24:54 -0700

    Added code for setup and teardown with BDD

--- .mocharc.js
+++ .mocharc.js
@@ -1,3 +1,2 @@
 module.exports = {
-    ui: 'tdd'
 }
--- test/app.spec.js
+++ test/app.spec.js
@@ -2,13 +2,32 @@ const { add, sub } = require('../src/app')
 const expect = require('chai').expect
 
 //BDD
-/*
+
 describe('Suite 1', () => {
+    afterEach(() => {
+        console.log('afterEach')
+    });
+    beforeEach(() => {
+        console.log('beforeEach')
+    });
+    after(() => {
+        console.log('after')
+    });
+    before(() => {
+        console.log('before');
+    })
+
     it('add(2, 3) should return 5', () =>{
         expect(add(2, 3)).to.be.equal(5);
+        console.log('it');
+    })
+
+    it('two', ()=>{
+        console.log('it two');
     })
 })
 
+/*
 describe('Suite 2', () => {
     it('add(3, 3) should return 6', () =>{
         expect(add(3, 3)).to.be.equal(6);
@@ -17,9 +36,10 @@ describe('Suite 2', () => {
 */
 //TDD
 //const { suite, test } = require('mocha')
-
+/*
 suite('Suite 1', () => {
     test('add(2,3) should return 5', () => {
         expect(add(2, 3)).to.be.equal(5);
     })
 })
+*/
